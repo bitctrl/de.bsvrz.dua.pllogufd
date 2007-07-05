3ccb314b95ef749bcd139db08994ea1ac202cef2
tfelder <info@bitctrl.de>
2007-07-05 15:30:52 +0000

    soweit ersmal fertig
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.pllogufd@2138 40c0be50-5d29-0410-901d-fb59edb5cafc

---
+++ src/de/bsvrz/dua/pllogufd/testmeteo/pub/Publikation.java
@@ -0,0 +1,141 @@
+/**
+ * Segment 4 Datenübernahme und Aufbereitung (DUA), SWE 4.3 Pl-Prüfung logisch UFD
+ * Copyright (C) 2007 BitCtrl Systems GmbH 
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Contact Information:<br>
+ * BitCtrl Systems GmbH<br>
+ * Weißenfelser Straße 67<br>
+ * 04229 Leipzig<br>
+ * Phone: +49 341-490670<br>
+ * mailto: info@bitctrl.de
+ */
+
+package de.bsvrz.dua.pllogufd.testmeteo.pub;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import stauma.dav.clientside.ResultData;
+import de.bsvrz.sys.funclib.bitctrl.dua.DUAInitialisierungsException;
+import de.bsvrz.sys.funclib.bitctrl.dua.adapter.AbstraktBearbeitungsKnotenAdapter;
+import de.bsvrz.sys.funclib.bitctrl.dua.av.DAVObjektAnmeldung;
+import de.bsvrz.sys.funclib.bitctrl.dua.dfs.DFSKonstanten;
+import de.bsvrz.sys.funclib.bitctrl.dua.dfs.schnittstellen.IDatenFlussSteuerung;
+import de.bsvrz.sys.funclib.bitctrl.dua.dfs.schnittstellen.IDatenFlussSteuerungFuerModul;
+import de.bsvrz.sys.funclib.bitctrl.dua.dfs.typen.ModulTyp;
+import de.bsvrz.sys.funclib.bitctrl.dua.schnittstellen.IStandardAspekte;
+import de.bsvrz.sys.funclib.bitctrl.dua.schnittstellen.IVerwaltung;
+
+/**
+ * In diesem Submodul findet lediglich die Publikation der Daten nach den 
+ * Vorgaben der Datenflusssteuerung für das Modul Pl-Prüfung logisch UFD
+ * statt 
+ * 
+ * @author BitCtrl Systems GmbH, Thierfelder
+ *
+ */
+public class Publikation
+extends AbstraktBearbeitungsKnotenAdapter{
+	
+	/**
+	 * Parameter zur Datenflusssteuerung für diese
+	 * SWE und dieses Modul
+	 */
+	private IDatenFlussSteuerungFuerModul iDfsMod = DFSKonstanten.STANDARD;
+	
+	
+	/**
+	 * Standardkonstruktor
+	 * 
+	 * @param stdAspekte Informationen zu den Standardpublikationsaspekten für diese
+	 * Instanz des Moduls Pl-Prüfung logisch UFD
+	 */
+	public Publikation(final IStandardAspekte stdAspekte){
+		this.standardAspekte = stdAspekte;
+	}
+	
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void initialisiere(IVerwaltung dieVerwaltung)
+	throws DUAInitialisierungsException {
+		super.initialisiere(dieVerwaltung);
+	
+		if(this.publizieren){
+			this.publikationsAnmeldungen.modifiziereObjektAnmeldung(this.standardAspekte.
+					getStandardAnmeldungen(this.verwaltung.getSystemObjekte()));
+		}
+	}		
+
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void aktualisiereDaten(ResultData[] resultate) {
+		if(this.publizieren){
+			if(resultate != null){
+				for(ResultData resultat:resultate){
+					if(resultat != null && resultat.getData() != null){
+						ResultData publikationsDatum = 
+							iDfsMod.getPublikationsDatum(resultat,
+								resultat.getData(), standardAspekte.getStandardAspekt(resultat));
+						if(publikationsDatum != null){
+							this.publikationsAnmeldungen.sende(publikationsDatum);
+						}			
+					}
+				}
+			}
+		}
+	}
+
+	
+	/**
+	 * {@inheritDoc} 
+	 */
+	public ModulTyp getModulTyp() {
+		return ModulTyp.PL_PRUEFUNG_LOGISCH_UFD;
+	}
+
+	
+	/**
+	 * {@inheritDoc} 
+	 */
+	public void aktualisierePublikation(IDatenFlussSteuerung iDfs) {
+		this.iDfsMod = iDfs.getDFSFuerModul(this.verwaltung.getSWETyp(),
+				this.getModulTyp());
+		
+		if(this.publizieren){
+			Collection<DAVObjektAnmeldung> anmeldungenStd =
+							new ArrayList<DAVObjektAnmeldung>();
+
+			if(this.standardAspekte != null){
+				anmeldungenStd = this.standardAspekte.
+									getStandardAnmeldungen(this.verwaltung.getSystemObjekte());
+			}
+			
+			Collection<DAVObjektAnmeldung> anmeldungen = 
+					this.iDfsMod.getDatenAnmeldungen(this.verwaltung.getSystemObjekte(), 
+							anmeldungenStd);
+			
+			synchronized(this){
+				this.publikationsAnmeldungen.modifiziereObjektAnmeldung(anmeldungen);
+			}
+		}
+	}
+}
---
+++ src/de/bsvrz/dua/pllogufd/testmeteo/pub/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Klassen für Submodul Publikation der Komponente Meteorologische
+ * Kontrolle innerhalb des Moduls Pl-Prüfung logisch UFD
+ */
+
+package de.bsvrz.dua.pllogufd.testmeteo.pub;
+
