88ae3e4d45e288be695d860dcdd35582e74c1d62
tfelder <info@bitctrl.de>
2007-07-05 15:29:43 +0000

    soweit ersmal fertig
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.pllogufd@2131 40c0be50-5d29-0410-901d-fb59edb5cafc

--- src/de/bsvrz/dua/pllogufd/plformal/package-info.java
+++ src/de/bsvrz/dua/pllogufd/plformal/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Klassen für Verwaltungsmodul der SWE Pl-Prüfung logisch UFD
+ * Klassen für Modul Pl-Prüfung formal innerhalb der SWE Pl-Prüfung logisch UFD
  */
 
 package de.bsvrz.dua.pllogufd.plformal;
