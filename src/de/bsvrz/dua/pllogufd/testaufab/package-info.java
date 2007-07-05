55689a39b933f688a3a517797b1c880e23735aec
tfelder <info@bitctrl.de>
2007-07-05 15:29:52 +0000

    soweit ersmal fertig
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.pllogufd@2132 40c0be50-5d29-0410-901d-fb59edb5cafc

--- src/de/bsvrz/dua/pllogufd/testaufab/package-info.java
+++ src/de/bsvrz/dua/pllogufd/testaufab/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Klassen für Verwaltungsmodul der SWE Pl-Prüfung logisch UFD
+ * Klassen für Modul Anstieg-Abfall-Kontrolle der SWE Pl-Prüfung logisch UFD
  */
 
 package de.bsvrz.dua.pllogufd.testaufab;
