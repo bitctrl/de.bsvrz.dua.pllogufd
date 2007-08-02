c49d780fc148c3b5564ac0c4b33a53e5745dadf3
tfelder <info@bitctrl.de>
2007-08-02 15:21:38 +0000

    Übernimmt die Publikation für ein Modul
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.pllogufd@2753 40c0be50-5d29-0410-901d-fb59edb5cafc

--- src/de/bsvrz/dua/pllogufd/testausfall/UFDAusfallUeberwachung.java
+++ src/de/bsvrz/dua/pllogufd/testausfall/UFDAusfallUeberwachung.java
@@ -26,32 +26,15 @@
 
 package de.bsvrz.dua.pllogufd.testausfall;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.TreeSet;
-
 import stauma.dav.clientside.ClientReceiverInterface;
 import stauma.dav.clientside.DataDescription;
 import stauma.dav.clientside.ReceiveOptions;
 import stauma.dav.clientside.ReceiverRole;
 import stauma.dav.clientside.ResultData;
-import stauma.dav.configuration.interfaces.SystemObject;
-import sys.funclib.debug.Debug;
 import de.bsvrz.sys.funclib.bitctrl.dua.DUAInitialisierungsException;
 import de.bsvrz.sys.funclib.bitctrl.dua.DUAKonstanten;
-import de.bsvrz.sys.funclib.bitctrl.dua.KontrollProzess;
-import de.bsvrz.sys.funclib.bitctrl.dua.adapter.AbstraktBearbeitungsKnotenAdapter;
-import de.bsvrz.sys.funclib.bitctrl.dua.dfs.schnittstellen.IDatenFlussSteuerung;
-import de.bsvrz.sys.funclib.bitctrl.dua.dfs.typen.ModulTyp;
-import de.bsvrz.sys.funclib.bitctrl.dua.schnittstellen.IKontrollProzessListener;
 import de.bsvrz.sys.funclib.bitctrl.dua.schnittstellen.IVerwaltung;
+import de.bsvrz.sys.funclib.bitctrl.dua.testausfall.AbstraktAusfallUeberwachung;
 import de.bsvrz.sys.funclib.bitctrl.dua.ufd.UmfeldDatenSensorDatum;
 import de.bsvrz.sys.funclib.bitctrl.konstante.Konstante;
 
@@ -71,41 +54,9 @@ import de.bsvrz.sys.funclib.bitctrl.konstante.Konstante;
  *
  */
 public class UFDAusfallUeberwachung
-extends AbstraktBearbeitungsKnotenAdapter 
-implements IKontrollProzessListener<Long>,
-		   ClientReceiverInterface{
-	
-	/**
-	 * Debug-Logger
-	 */
-	private static final Debug LOGGER = Debug.getLogger();
-	
-	/**
-	 * Eine Map mit allen aktuellen Kontrollzeitpunkten und den zu diesen
-	 * Kontrollzeitpunkten zu überprüfenden Umfelddatensensoren
-	 */
-	private SortedMap<Long, Collection<AusfallUFDSDatum>> kontrollZeitpunkte =
-					Collections.synchronizedSortedMap(new TreeMap<Long, Collection<AusfallUFDSDatum>>());
-	
-	/**
-	 * Mapt alle betrachteten Umfelddatensensoren auf den aktuell für sie
-	 * erlaubten maximalen Zeitverzug
-	 */
-	private Map<SystemObject, Long> sensorWertErfassungVerzug =
-					Collections.synchronizedMap(new TreeMap<SystemObject, Long>());
-
-	/**
-	 * interner Kontrollprozess
-	 */
-	private KontrollProzess<Long> kontrollProzess = null;
-	
-	/**
-	 * speichert pro Umfelddatensensor die letzte empfangene Datenzeit
-	 */
-	private Map<SystemObject, Long> letzteEmpfangeneDatenZeitProSensor = new HashMap<SystemObject, Long>(); 
-	
-	
-	
+extends AbstraktAusfallUeberwachung 
+implements ClientReceiverInterface{
+			
 		
 	/**
 	 * {@inheritDoc}
@@ -114,237 +65,43 @@ implements IKontrollProzessListener<Long>,
 	public void initialisiere(IVerwaltung dieVerwaltung)
 	throws DUAInitialisierungsException {
 		super.initialisiere(dieVerwaltung);
-		this.kontrollProzess = new KontrollProzess<Long>();
-		this.kontrollProzess.addListener(this);
 		
 		DataDescription parameterBeschreibung = new DataDescription(
 				dieVerwaltung.getVerbindung().getDataModel().getAttributeGroup("atg.ufdsAusfallÜberwachung"), //$NON-NLS-1$
 				dieVerwaltung.getVerbindung().getDataModel().getAspect(Konstante.DAV_ASP_PARAMETER_SOLL),
 				(short)0);
 		dieVerwaltung.getVerbindung().subscribeReceiver(this, dieVerwaltung.getSystemObjekte(),
-				parameterBeschreibung, ReceiveOptions.normal(), ReceiverRole.receiver());
-		
-		for(SystemObject sensor:verwaltung.getSystemObjekte()){
-			this.letzteEmpfangeneDatenZeitProSensor.put(sensor, new Long(-1));
-		}
+				parameterBeschreibung, ReceiveOptions.normal(), ReceiverRole.receiver());		
+	}
+
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected ResultData getAusfallDatumVon(ResultData originalResultat) {
+		UmfeldDatenSensorDatum wert = new UmfeldDatenSensorDatum(originalResultat);
+		wert.setStatusErfassungNichtErfasst(DUAKonstanten.JA);
+		wert.getWert().setNichtErmittelbarAn();
+
+		long zeitStempel = wert.getDatenZeit() + wert.getT();
+
+		ResultData resultat = new ResultData(originalResultat.getObject(), 
+				originalResultat.getDataDescription(), zeitStempel, wert.getDatum());
+
+		return resultat;
 	}
 
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public synchronized void aktualisiereDaten(ResultData[] resultate) {
-		if(resultate != null){
-			List<ResultData> weiterzuleitendeResultate = new ArrayList<ResultData>();
-			
-			for(ResultData resultat:resultate){
-				if(resultat != null){
-					
-					/**
-					 * Hier werden die Daten herausgefiltert, die von der Ausfallkontrolle
-					 * quasi zu unrecht generiert wurden, da das Datum nur minimal zu spät kam.
-					 */
-					if(this.letzteEmpfangeneDatenZeitProSensor.get(resultat.getObject()) < resultat.getDataTime()){
-						/**
-						 * Zeitstempel ist echt neu!
-						 */
-						weiterzuleitendeResultate.add(resultat);
-					}
-					this.letzteEmpfangeneDatenZeitProSensor.put(resultat.getObject(), resultat.getDataTime());
-					
-					
-					if(resultat.getData() != null){
-											
-						this.bereinigeKontrollZeitpunkte(resultat);
-						
-						long kontrollZeitpunkt = this.getKontrollZeitpunktVon(resultat);
-						if(kontrollZeitpunkt > 0){
-							Collection<AusfallUFDSDatum> kontrollObjekte =
-									this.kontrollZeitpunkte.get(kontrollZeitpunkt);
-								
-							/**
-							 * Kontrolldatum bestimmten
-							 */
-							AusfallUFDSDatum neuesKontrollObjekt = new AusfallUFDSDatum(resultat);							
-							if(kontrollObjekte != null){
-								kontrollObjekte.add(neuesKontrollObjekt);
-							}else{
-								kontrollObjekte = new TreeSet<AusfallUFDSDatum>();
-								kontrollObjekte.add(neuesKontrollObjekt);
-								this.kontrollZeitpunkte.put(new Long(kontrollZeitpunkt), kontrollObjekte);
-							}
-						}
-	
-						long fruehesterKontrollZeitpunkt = -1;
-												
-						if(!this.kontrollZeitpunkte.isEmpty()){
-							fruehesterKontrollZeitpunkt = this.kontrollZeitpunkte.firstKey().longValue();
-							
-							if(fruehesterKontrollZeitpunkt > 0){
-								this.kontrollProzess.setNaechstenAufrufZeitpunkt(fruehesterKontrollZeitpunkt,
-																				 new Long(fruehesterKontrollZeitpunkt));
-							}else{
-								LOGGER.warning("Der momentan aktuellste Kontrollzeitpunkt ist <= 0"); //$NON-NLS-1$
-							}
-						}else{
-							LOGGER.warning("Die Menge der Kontrollzeitpunkte ist leer"); //$NON-NLS-1$
-						}
-					}
-				}
-			}
-			
-			if(this.knoten != null && !weiterzuleitendeResultate.isEmpty()){
-				this.knoten.aktualisiereDaten(weiterzuleitendeResultate.toArray(new ResultData[0]));
-			}
-		}
-	}
-	
-	
-	/**
-	 * Bereinigt die Liste der Kontrollzeitpunkte anhand des gerade eingetroffenen
-	 * Datums. Dabei wird zunächst der momentan noch erwartete Kontrollzeitpunkt dieses
-	 * Datums berechnet und dieser dann aus der Liste der Kontrollzeitpunkte entfernt
-	 * 
-	 * @param resultat ein Roh-Datum eines Umfelddatensensors
-	 */
-	@SuppressWarnings("null")
-	private final void bereinigeKontrollZeitpunkte(final ResultData resultat){
-		
-		/**
-		 * Berechne den wahrscheinlichsten Zeitpunkt, für den hier noch auf 
-		 * ein Datum dieses Objektes gewartet wird 
-		 */
-		final Long letzterErwarteterZeitpunkt = resultat.getDataTime() + 
-												new UmfeldDatenSensorDatum(resultat).getT() +
-												this.getMaxZeitVerzug(resultat.getObject());
-		
-		Collection<AusfallUFDSDatum> kontrollObjekte = 
-			this.kontrollZeitpunkte.get(letzterErwarteterZeitpunkt);
-		
-		AusfallUFDSDatum datum = new AusfallUFDSDatum(resultat);
-
-		/**
-		 * Gibt es einen Kontrollzeitpunkt, für den das Objekt, des empfangenen Datums
-		 * eingeplant sein müsste
-		 */
-		if(kontrollObjekte != null){
-			if(kontrollObjekte.remove(datum)){
-				if(kontrollObjekte.isEmpty()){
-					this.kontrollZeitpunkte.remove(letzterErwarteterZeitpunkt);
-				}
-			}else{
-				kontrollObjekte = null;
-			}
-		}
-
-		if(kontrollObjekte == null){
-			Long gefundenInKontrollZeitpunkt = new Long(-1);
-			for(Long kontrollZeitpunkt:this.kontrollZeitpunkte.keySet()){
-				kontrollObjekte = this.kontrollZeitpunkte.get(kontrollZeitpunkt);
-				if(kontrollObjekte.remove(datum)){
-					gefundenInKontrollZeitpunkt = kontrollZeitpunkt;
-					break;
-				}
-			}
-
-			if(gefundenInKontrollZeitpunkt >= 0){
-				if(kontrollObjekte.isEmpty()){
-					this.kontrollZeitpunkte.remove(gefundenInKontrollZeitpunkt);
-				}					
-			}else{
-				LOGGER.info("Datum " + datum + " konnte nicht aus" + //$NON-NLS-1$ //$NON-NLS-2$
-				" Kontrollwarteschlange gelöscht werden"); //$NON-NLS-1$
-			}
-		}
-	}
-	
-	
-	/**
-	 * Erfragt den maximalen Zeitverzug für einen Umfelddatensensor
-	 * 
-	 * @param obj ein Umfelddatensensor
-	 * @return der maximale Zeitverzug für den Umfelddatensensor oder
-	 * -1, wenn dieser nicht ermittelt werden konnte
-	 */
-	private final long getMaxZeitVerzug(final SystemObject obj){
-		long maxZeitVerzug = -1;
-
-		if(obj != null){
-			synchronized (this.sensorWertErfassungVerzug) {
-				Long dummy = this.sensorWertErfassungVerzug.get(obj);
-				if(dummy != null && dummy > 0){
-					maxZeitVerzug = dummy;
-				}
-			}
-		}
-		
-		return maxZeitVerzug;
-	}
-	
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public void trigger(Long kontrollZeitpunkt) {
-		List<ResultData> zuSendendeAusfallDatenMenge = new ArrayList<ResultData>();
-		
-		synchronized (this.kontrollZeitpunkte) {
-			Collection<AusfallUFDSDatum> ausfallDaten = this.kontrollZeitpunkte.get(kontrollZeitpunkt);
-			if(ausfallDaten != null){
-				for(AusfallUFDSDatum ausfallDatum:ausfallDaten){				
-					UmfeldDatenSensorDatum wert = new UmfeldDatenSensorDatum(ausfallDatum.getDatum());
-					wert.setStatusErfassungNichtErfasst(DUAKonstanten.JA);
-					wert.getWert().setNichtErmittelbarAn();
-
-					long zeitStempel = wert.getDatenZeit() + wert.getT();
-
-					ResultData resultat = new ResultData(ausfallDatum.getDatum().getObject(), 
-							ausfallDatum.getDatum().getDataDescription(), zeitStempel, wert.getDatum());
-
-					zuSendendeAusfallDatenMenge.add(resultat);
-				}				
-			}else{
-				LOGGER.warning("Der Kontrollzeitpunkt " +  //$NON-NLS-1$
-						DUAKonstanten.ZEIT_FORMAT_GENAU.format(new Date(kontrollZeitpunkt)) + 
-						" wurde inzwischen entfernt"); //$NON-NLS-1$
-			}
-		}
-		
-		/**
-		 * Sende die ausgefallenen Daten an mich selbst, um
-		 * die Liste der Ausfallzeitpunkte zu aktualisieren
-		 */
-		if(zuSendendeAusfallDatenMenge.size() > 0){
-			this.aktualisiereDaten(zuSendendeAusfallDatenMenge.toArray(new ResultData[0]));
-		}		
+	@Override
+	protected long getTVon(ResultData resultat) {
+		UmfeldDatenSensorDatum datum = new UmfeldDatenSensorDatum(resultat);
+		return datum.getT();
 	}
 
-	
-	/**
-	 * Erfragt den Zeitpunkt, zu dem von dem Objekt, das mit diesem 
-	 * Datensatz assoziiert ist, ein neuer Datensatz (spätestens) erwartet wird
-	 * 
-	 * @param empfangenesResultat ein empfangener Datensatz  
-	 * @return der späteste Zeitpunkt des nächsten Datensatzes oder -1, 
-	 * wenn dieser nicht sinnvoll bestimmt werden konnte (wenn z.B. keine
-	 * Parameter vorliegen)
-	 */
-	private final long getKontrollZeitpunktVon(final ResultData empfangenesResultat){
-		long kontrollZeitpunkt = -1;
-
-		long maxZeitVerzug = this.getMaxZeitVerzug(empfangenesResultat.getObject());
-
-		if(maxZeitVerzug >= 0){
-			UmfeldDatenSensorDatum datum = new UmfeldDatenSensorDatum(empfangenesResultat);
-			kontrollZeitpunkt = empfangenesResultat.getDataTime() + 2 * datum.getT() + maxZeitVerzug;
-		}else{
-			LOGGER.fine("Es wurden noch keine (sinnvollen) Parameter empfangen: " //$NON-NLS-1$ 
-					+ empfangenesResultat.getObject());
-		}
-
-		return kontrollZeitpunkt;
-	}
-	
 	
 	/**
 	 * {@inheritDoc}
@@ -353,8 +110,8 @@ implements IKontrollProzessListener<Long>,
 		if(resultate != null){
 			for(ResultData resultat:resultate){
 				if(resultat != null && resultat.getData() != null){
-					synchronized (this.sensorWertErfassungVerzug) {
-						this.sensorWertErfassungVerzug.put(resultat.getObject(), 
+					synchronized (this.objektWertErfassungVerzug) {
+						this.objektWertErfassungVerzug.put(resultat.getObject(), 
 								new Long(resultat.getData().getTimeValue("maxZeitVerzug").getMillis())); //$NON-NLS-1$
 						LOGGER.info("Neue Parameter: maxZeitVerzug(" + resultat.getObject() + ") = " +  //$NON-NLS-1$ //$NON-NLS-2$
 								resultat.getData().getTimeValue("maxZeitVerzug").getMillis() + "ms"); //$NON-NLS-1$ //$NON-NLS-2$
@@ -362,21 +119,5 @@ implements IKontrollProzessListener<Long>,
 				}
 			}
 		}
-	}
-
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public ModulTyp getModulTyp() {
-		return null;
-	}
-
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public void aktualisierePublikation(IDatenFlussSteuerung dfs) {
-		// hier wird nicht publiziert
 	}
 }
