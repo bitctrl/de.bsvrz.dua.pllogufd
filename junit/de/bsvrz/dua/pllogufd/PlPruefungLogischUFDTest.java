c96452ea333c886fb767b693ad796555ea5bc63c
tfelder <info@bitctrl.de>
2008-01-25 15:47:34 +0000

    Anpassung an neue Kernsoftware
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.pllogufd@5922 40c0be50-5d29-0410-901d-fb59edb5cafc

--- junit/de/bsvrz/dua/pllogufd/PlPruefungLogischUFDTest.java
+++ junit/de/bsvrz/dua/pllogufd/PlPruefungLogischUFDTest.java
@@ -65,7 +65,7 @@ implements ClientSenderInterface{
 	public static final String[] CON_DATA = new String[] {
 			"-datenverteiler=localhost:8083", //$NON-NLS-1$ 
 			"-benutzer=Tester", //$NON-NLS-1$
-			"-authentifizierung=c:\\passwd1", //$NON-NLS-1$
+			"-authentifizierung=c:\\passwd", //$NON-NLS-1$
 			"-debugLevelStdErrText=INFO", //$NON-NLS-1$
 			"-debugLevelFileText=INFO" }; //$NON-NLS-1$
 	
